02e3be830a6b319fef2d7f2ac98c29569a3b8e68
JonathanWaller <waller.j27@gmail.com>
2019-01-02 20:59:27 -0600

    adding components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from "react";
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: "purple" }}>
+    <h1> Hello Gatsby!</h1>
+    <p>What a world</p>
+    <img
+      src="http://www.pmlive.com/__data/assets/image/0005/1245524/Horizon.JPG"
+      alt=""
+    />
+  </div>
+);
